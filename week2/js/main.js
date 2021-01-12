a25c69a3e90e1a5d0016d4e2c312165389e30cbe
Melissa Hull <melissahull87@gmail.com>
2021-01-11 19:16:53 -0600

    change div color

--- week2/js/main.js
+++ week2/js/main.js
@@ -1,3 +1,9 @@
 function clickme() {
     alert('Clicked!');
 }
+
+function changecolor() {
+    var color = document.getElementById('color').innerText;
+    var div = document.getElementById('div1');
+    div.style.backgroundColor = color;
+}
