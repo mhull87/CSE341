d417a515ab2c84537656b935558012efcff87f0c
Melissa Hull <melissahull87@gmail.com>
2021-01-11 19:31:48 -0600

    gfd

--- week2/js/main.js
+++ week2/js/main.js
@@ -3,7 +3,7 @@ function clickme() {
 }
 
 function changecolor() {
-    var color = document.getElementById('color').innerText;
+    var color = document.getElementById('color').value;
     var div = document.getElementById('div1');
     div.style.backgroundColor = color;
 }
