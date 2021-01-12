f3b8ac66b32552d34e8f5f921c766a9ead3630e8
Melissa Hull <melissahull87@gmail.com>
2021-01-12 13:54:19 -0600

    group assignment 1

--- week2/js/main.js
+++ week2/js/main.js
@@ -3,7 +3,18 @@ function clickme() {
 }
 
 function changecolor() {
-    var color = document.getElementById('color').value;
-    var div = document.getElementById('div1');
-    div.style.backgroundColor = color;
+    // var color = document.getElementById('color').value;
+    // var div = document.getElementById('div1');
+    // div.style.backgroundColor = color;
 }
+
+
+
+// $(function() {
+
+//   $("#btn").click(function() {
+//     var change = $("#color").val();
+//     $("#div1").css("background-color", change);
+//    });
+   
+//   });
