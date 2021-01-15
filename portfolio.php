8e4b79bbf39d5e5ff8946fabe4a0608b5d051531
Melissa Hull <melissahull87@gmail.com>
2021-01-14 20:01:03 -0600

    fghjk

--- portfolio.php
+++ portfolio.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Web Engineering II Portfolio</title>
-  <link href="../css/main.css" rel="stylesheet">
+  <link href="css/main.css" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Bad+Script&family=Staatliches&display=swap" rel="stylesheet">
+
 </head>
-
 <body>
-  <header>
+  <h1>Melissa Hull's Portfolio</h1>
+  <ul>
+    <li><a href="week2/index.html">Week 2</a></li>
+  </ul>
+  <button type='button' onclick="location.href = 'index.html'">Go Back Home</button>
 
-  </header>
-
-  <nav>
-    <? include $_SERVER['DOCUMENT_ROOT'].'/common/home.php'; ?>
-  </nav>
+</body>
+</html>
