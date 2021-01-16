025f6b8dad18248620daa587420310d5dfe1cc10
Melissa Hull <melissahull87@gmail.com>
2021-01-16 15:09:39 -0600

    added a button

--- web/common/footer.php
+++ web/common/footer.php
@@ -1,6 +1,10 @@
 <footer>
 <p class="btn">
-    <button type='button' onclick="location.href = 'portfolio.php'">View My Portfolio</button>
+    <button type='button' onclick="location.href = 'portfolio.php'" title="View My Portfolio">View My Portfolio</button>
+  </p>
+
+  <p class="btn">
+    <button type='button' onclick="location.href = 'home.php'" title="Go TO Home Page">Home Page</button>
   </p>
 </footer>
 
