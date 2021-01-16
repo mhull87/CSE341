b727a7e3f8dccd7f8db8b80ff8ea15c12d422801
Melissa Hull <melissahull87@gmail.com>
2021-01-16 13:15:47 -0600

    created home page

---
+++ web/home.php
@@ -0,0 +1,18 @@
+<?php include $_SERVER['DOCUMENT_ROOT'].'/cse341/web/common/header.php'; ?>
+
+<main>
+  <h1>Melissa Hull</h1>
+  <img src="images/profileresize.jpg" class='profileimg' alt="Melissa's Profile Picture">
+  <p>Hi, I’m Melissa Hull. I’m currently studying my last semester at Brigham Young University-Idaho. My major is
+    Professional Studies with an emphasis in Web Development and Family History Research. Front-end development is my
+    passion and with my vast knowledge of server-side programming, I have a notable ability to communicate with the
+    back-end as well. I work well on my own but I thrive in a team environment and do my best work by breaking down
+    problems to solve them. Being a Home Engineer for over ten years, I have the ability to work under pressure and have
+    remarkable scheduling and organizational skills. I’d really love to work for you.</p>
+    
+  <p class="btn">
+    <button type='button' onclick="location.href = 'portfolio.php'">View My Portfolio</button>
+  </p>
+</main>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/cse341/web/common/footer.php'; ?>
---
+++ web/portfolio.php
@@ -0,0 +1,12 @@
+<?php include $_SERVER['DOCUMENT_ROOT'].'/cse341/web/common/header.php'; ?>
+
+<h1>Melissa Hull's Portfolio</h1>
+<ul>
+  <li><a href="week2/index.html">Week 2</a></li>
+</ul>
+
+<p class="btn">
+  <button type='button' onclick="location.href = 'home.php'">Go Back Home</button>
+</p>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/cse341/web/common/footer.php'; ?>
