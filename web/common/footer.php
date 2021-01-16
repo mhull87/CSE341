e59083e811fa8dc3269f694f993c6cf5bf08f0f7
Melissa Hull <melissahull87@gmail.com>
2021-01-16 15:11:44 -0600

    one last change

--- web/common/footer.php
+++ web/common/footer.php
@@ -3,9 +3,10 @@
     <button type='button' onclick="location.href = 'portfolio.php'" title="View My Portfolio">View My Portfolio</button>
   </p>
 
+<!--   use js to hide the p tag that will not be seen on the page
   <p class="btn">
     <button type='button' onclick="location.href = 'home.php'" title="Go TO Home Page">Home Page</button>
-  </p>
+  </p> -->
 </footer>
 
 </body>
