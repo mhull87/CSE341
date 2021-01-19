bb74d1e5e4e6129ca16f1744822242fc10683026
Melissa Hull <melissahull87@gmail.com>
2021-01-18 22:22:47 -0600

    lets try this

--- web/week3/form.php
+++ web/week3/form.php
@@ -1,17 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="utf-8" />
+  <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>Week 3 Team Activity</title>
   <link href="../css/main.css" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Bad+Script&family=Staatliches&display=swap" rel="stylesheet">
 </head>
+
 <body>
   <p>Name: <?php echo $_POST["name"]; ?></p>
   <p>Email: <a href="mailto:<?php echo $_POST["email"]; ?>"><?php echo$_POST["email"]; ?></a></p>
   <p>Major: <?php echo $_POST["major"]; ?></p>
   <p>Comments: <?php echo $_POST["comments"]; ?></p>
-  <p>Continents Visited: <?php echo $_POST["continents[]"]; ?></p>
+  <p>Continents Visited: <?php foreach($_POST["continents"] as $selected){
+    echo $selected. "</br>"; ?>
+    }</p>
 </body>
+
 </html>
