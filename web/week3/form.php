5339332545757bd72c6c2354623eacd78db907f7
Melissa Hull <melissahull87@gmail.com>
2021-01-18 20:51:03 -0600

    basic form for week 3 team activity

---
+++ web/week3/form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" type="text/css" href="css/main.css" />
+  <title>Week 3 Team Activity</title>
+  <script type="text/javascript" src="js/main.js"></script>
+  <script src="https://code.jquery.com/jquery-3.1.1.js"></script>
+  <link href="../css/main.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Bad+Script&family=Staatliches&display=swap" rel="stylesheet">
+</head>
+<body>
+  <p><?php echo $_POST["name"]; ?></p>
+  <p><?php echo $_POST["email"]; ?></p>
+  <p><?php echo $_POST["major"]; ?></p>
+  <p><?php echo $_POST["comments"]; ?></p>
+</body>
+</html>
