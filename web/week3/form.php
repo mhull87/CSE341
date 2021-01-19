fa26efb0d4fd64dc6c2c9a1744d24f6cd25f2066
Melissa Hull <melissahull87@gmail.com>
2021-01-18 21:26:15 -0600

    dfghjkl

--- web/week3/form.php
+++ web/week3/form.php
@@ -3,16 +3,13 @@
 <head>
 <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link rel="stylesheet" type="text/css" href="css/main.css" />
   <title>Week 3 Team Activity</title>
-  <script type="text/javascript" src="js/main.js"></script>
-  <script src="https://code.jquery.com/jquery-3.1.1.js"></script>
   <link href="../css/main.css" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Bad+Script&family=Staatliches&display=swap" rel="stylesheet">
 </head>
 <body>
   <p><?php echo $_POST["name"]; ?></p>
-  <p><a href="mailto:<?php echo $_POST["email"]; ?>"></a></p>
+  <p><a href="mailto:<?php echo $_POST["email"]; ?>"><?php echo$_POST["email"]; ?></a></p>
   <p><?php echo $_POST["major"]; ?></p>
   <p><?php echo $_POST["comments"]; ?></p>
 </body>
