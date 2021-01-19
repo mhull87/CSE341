d1a9c79aef0c01f3f58992219ed3bd29840c6d4e
Melissa Hull <melissahull87@gmail.com>
2021-01-19 13:13:52 -0600

    link fix

--- web/common/footer.php
+++ web/common/footer.php
@@ -1,6 +1,6 @@
 <footer>
 <p class="btn">
-    <button type='button' onclick="location.href = 'portfolio.php'" title="View My Portfolio">View My Portfolio</button>
+    <button type='button' onclick="location.href = '../home/portfolio.php'" title="View My Portfolio">View My Portfolio</button>
   </p>
 </footer>
 
