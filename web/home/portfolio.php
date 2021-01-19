8d4bfb2886626f1d3f0a41e95b648ce5c573168d
Melissa Hull <melissahull87@gmail.com>
2021-01-19 10:48:59 -0600

    dfghj

--- web/home/portfolio.php
+++ web/home/portfolio.php
@@ -1,4 +1,4 @@
-<?php include 'common/header.php'; ?>
+<?php include '../common/header.php'; ?>
 
 <h1>Melissa Hull's Portfolio</h1>
 <ul>
--- web/week3/form.php
+++ web/week3/form.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Week 3 Team Activity</title>
-  <link href="../css/main.css" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css2?family=Bad+Script&family=Staatliches&display=swap" rel="stylesheet">
-</head>
+<?php include '../common/header.php'; ?>
 
 <body>
   <p>Name: <?php echo $_POST["name"]; ?></p>
