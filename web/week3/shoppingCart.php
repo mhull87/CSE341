4b59bea8064ef3f03a35e2e6e885260559735faa
Melissa Hull <melissahull87@gmail.com>
2021-01-22 20:05:08 -0600

    switching projects

--- web/week3/shoppingCart.php
+++ web/week3/shoppingCart.php
@@ -2,21 +2,29 @@
 
 include $_SERVER['DOCUMENT_ROOT']."/cse341/web/common/header.php"; ?>
 
+
 <main>
 <h1>Your Shopping Cart</h1>
 <?php 
 foreach($_POST['selection'] as $selected)
-{
   $_SESSION[$selected] = $selected;
-  //echo $selected."<br>";
-}
+//echo print_r($_SESSION);
+foreach($_SESSION as $key=>$val)
+echo '<p id="'.$key.'">'.$val.'<button id="'.$key.'" onclick="removeItem('.$key.')">Remove</button></p><br>';
 
-print_r($_SESSION);
 ?>
 
 <footer>
 <button onclick="location.href = 'browse.php'" title="Continue Shopping">Continue Shopping</button>
 </footer>
 </main>
+<script>
+function removeItem(key) {
+  <?php 
+  unset ($_SESSION[$key])
+  ?>
+location.reload();
+}
+</script>
 </body>
 </html>
