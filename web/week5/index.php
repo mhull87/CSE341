982b8c454d51f389c000ac740aa533f49b30bb66
Melissa Hull <melissahull87@gmail.com>
2021-02-01 11:15:02 -0600

    added querie

--- web/week5/index.php
+++ web/week5/index.php
@@ -28,4 +28,13 @@ foreach ($db->query('SELECT username, password FROM note_user') as $row)
   echo ' password: '.$row['password'];
   echo '<br/>';
 }
+
+$statement = $db->query('SELECT username, password FROM note_user');
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+  echo 'user: '.$row['username'].' password: '.$row['password'].'<br/>';
+}
+
+$statement = $db->query('SELECT username, passowrd FROM note_user');
+$results = $statement->fetchAll(PDO::FETCH_ASSOC);
 ?>
