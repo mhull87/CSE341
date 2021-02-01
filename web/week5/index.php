80963e80da57f40fd05950f47758949406057a30
Melissa Hull <melissahull87@gmail.com>
2021-02-01 11:26:53 -0600

    kjhg

--- web/week5/index.php
+++ web/week5/index.php
@@ -37,6 +37,16 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 }
 
 $statement = $db->query('SELECT username, passowrd FROM note_user');
-$results = $statement->fetchAll(PDO::FETCH_ASSOC);
-echo $results;
+$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+$stmt = $db->prepare('SELECT * FROM table WHERE id=:id AND name=:name');
+$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);
+$stmt->execute();
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$stmt = $db->prepare('SELECT * FROM table WHERE id=:id AND name=:name');
+$stmt->execute(array(':name' => $name, ':id' => $id));
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
