f571e2db44a67503e524856cf3f00d0405253fae
Melissa Hull <melissahull87@gmail.com>
2021-02-05 11:33:32 -0600

    jhgf

---
+++ web/bugout/common/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bug Out Survival</title>
+  <link href="../css/main.css" type="text/css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Bad+Script&family=Staatliches&display=swap" rel="stylesheet">
+</head>
+
+<body>
+  <header>
+<h1>Bug Out Survival</h1>
+  </header>
