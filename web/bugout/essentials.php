3daa5fe4044183ba3b52399816b22d9eb8f25454
Melissa Hull <melissahull87@gmail.com>
2021-02-05 17:42:37 -0600

    jhg

--- web/bugout/essentials.php
+++ web/bugout/essentials.php
@@ -45,7 +45,7 @@ include 'common/header.php';
 
         echo "<li>$name<br>
         <form action='details.php' method='POST'>
-        <input type'hidden' name='id' value='$id'>
+        <input type='hidden' name='id' value='$id'>
         <input type='submit' value='Details' name='details' id='details'>
         </form>
         </li>";
