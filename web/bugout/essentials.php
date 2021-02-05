827e2aefdc66f84b910fd4c33fe634dc11b4464b
Melissa Hull <melissahull87@gmail.com>
2021-02-05 14:56:53 -0600

    fghj

--- web/bugout/essentials.php
+++ web/bugout/essentials.php
@@ -1,6 +1,26 @@
 <?php
-require_once('connections/dbConnect.php');
-$db = get_db();
+//require_once('connections/dbConnect.php');
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
 
 $query = 'SELECT item_name, item_use FROM items';
 $stmt = $db->prepare($query);
