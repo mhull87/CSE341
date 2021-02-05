698d6d2d7801cef0afd57fea2064052ddf3ebe94
Melissa Hull <melissahull87@gmail.com>
2021-02-05 16:11:40 -0600

    nhbgf

--- web/bugout/mybag.php
+++ web/bugout/mybag.php
@@ -1,30 +1,30 @@
 <?php
-//require_once 'connections/dbConnect.php';
-//$db = get_db();
+require_once 'connections/dbConnect.php';
+$db = get_db();
 
-try
-{
-  $dbUrl = getenv('DATABASE_URL');
+// try
+// {
+//   $dbUrl = getenv('DATABASE_URL');
 
-  $dbOpts = parse_url($dbUrl);
+//   $dbOpts = parse_url($dbUrl);
 
-  $dbHost = $dbOpts["host"];
-  $dbPort = $dbOpts["port"];
-  $dbUser = $dbOpts["user"];
-  $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"],'/');
+//   $dbHost = $dbOpts["host"];
+//   $dbPort = $dbOpts["port"];
+//   $dbUser = $dbOpts["user"];
+//   $dbPassword = $dbOpts["pass"];
+//   $dbName = ltrim($dbOpts["path"],'/');
 
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+//   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
+//   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// }
+// catch (PDOException $ex)
+// {
+//   echo 'Error!: ' . $ex->getMessage();
+//   die();
+// }
 
-$query = 'SELECT i.item_name, b.packed, b.quantity, i.item_use
+$query = 'SELECT i.item_name, b.packed, b.quantity, i.item_use, b.location
           FROM bugout_bag b JOIN items i ON b.item_id = i.item_id';
 $stmt = $db->prepare($query);
 $stmt->execute();
@@ -47,7 +47,7 @@ include 'common/header.php';
       $quantity = $item['quantity'];
       $use = $item['use'];
 
-      echo "<li><p>$name</p><p>$packed</p><p>$quantity</p><p>$use</p></li>";
+      echo "<li><p>Item: $name<br>Packed: $packed<br>Quantity: $quantity<br>Use: $use</p></li>";
     }
   ?>
   </ul>
@@ -57,7 +57,21 @@ include 'common/header.php';
   <h3>My Extras</h3>
 
   <!-- list out the extras they have -->
+  <ul>
+    <?php 
+    foreach ($items as $item)
+    {
+      $name = $item['item_name'];
+      $packed = $item['packed'];
+      $quantity = $item['quantity'];
+      $use = $item['use'];
+      $location = $item['location'];
 
+      echo "<li><p>Item: $name<br>Packed: $packed<br>Quantity: $quantity<br>Use: $use<br>Location: $location</p></li>";
+    }
+?>
+
+    </ul>
   <a href="addtomyextras.php">My Extras</a>
 </main>
 
