8f79880fcb9a330cbbe03c6bf632bad00df52eac
Melissa Hull <melissahull87@gmail.com>
2021-02-08 17:26:16 -0600

    sdfghj

--- web/bugout/common/header.php
+++ web/bugout/common/header.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Bug Out Survival</title>
-  <link href="css/main.css" type="text/css" rel="stylesheet">
+  <link href="/css/main.css" type="text/css" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Bad+Script&family=Staatliches&display=swap" rel="stylesheet">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script>
