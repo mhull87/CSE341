d521c816fafc4f2292e46ede7a4ea70c19941663
Melissa Hull <melissahull87@gmail.com>
2021-02-08 20:14:18 -0600

    jhg

--- web/bugout/common/nav.php
+++ web/bugout/common/nav.php
@@ -1,5 +1,5 @@
 <ul>
   <li><a href="/web/bugout/index.php">Home</a></li>
-  <li><a href="/web//bugout/essentials.php">Survival Essentials</a></li>
+  <li><a href="/web/bugout/essentials.php">Survival Essentials</a></li>
   <li><a href="/web/bugout/mygear.php">My Gear</a></li>
 </ul>
