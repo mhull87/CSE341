366b4fea7a86adc3d9c8d43e12f7afa60179bc9a
Melissa Hull <melissahull87@gmail.com>
2021-02-11 12:45:15 -0600

    jhgf

--- web/bugout/essentials/index.php
+++ web/bugout/essentials/index.php
@@ -44,6 +44,27 @@ switch ($action)
     include $_SERVER['DOCUMENT_ROOT'].'/bugout/view/essentials.php';
     break;
 
+  case 'details':
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $use = $_POST['use'];
+
+    $details = "<p>Item #: $id<br>Name: $name<br>Use: $use</p>
+    <form action='addtobag.php' method='POST'>
+    <input type='hidden' name='id' value='$id'>
+    <input type='hidden' name='name' value='$name'>
+    <input type='hidden' name='use' value='$use'>
+    <input type='submit' value='Add To Bag'>
+    </form>
+    <form action='addtomyextras.php' method='POST'>
+    <input type='hidden' name='id' value='$id'>
+    <input type='hidden' name='name' value='$name'>
+    <input type='hidden' name='use' value='$use'>
+    <input type='submit' value='Add To Extras'>
+    </form>";
+    include '../view/details.php';
+    break;
+
   default:
     include '../index.php';
 }
--- web/bugout/view/details.php
+++ web/bugout/view/details.php
@@ -1,28 +1,12 @@
 <?php
-  $id = $_POST['id'];
-  $name = $_POST['name'];
-  $use = $_POST['use'];
-
 include $_SERVER['DOCUMENT_ROOT'].'/bugout/common/header.php';
 ?>
 
 <main>
   <h2>Item Details</h2>
 
-<?php
-    echo "<p>Item #: $id<br>Name: $name<br>Use: $use</p>
-    <form action='addtobag.php' method='POST'>
-    <input type='hidden' name='id' value='$id'>
-    <input type='hidden' name='name' value='$name'>
-    <input type='hidden' name='use' value='$use'>
-    <input type='submit' value='Add To Bag'>
-    </form>
-    <form action='addtomyextras.php' method='POST'>
-    <input type='hidden' name='id' value='$id'>
-    <input type='hidden' name='name' value='$name'>
-    <input type='hidden' name='use' value='$use'>
-    <input type='submit' value='Add To Extras'>
-    </form>";
+  <?php
+  echo $details;
   ?>
 </main>
 
