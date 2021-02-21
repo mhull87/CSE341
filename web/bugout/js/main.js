1ebd711a529f1b2629ffc5176a97fca66788c457
Melissa Hull <melissahull87@gmail.com>
2021-02-20 19:29:30 -0600

    hgfd

--- web/bugout/js/main.js
+++ web/bugout/js/main.js
@@ -1,14 +1,20 @@
 function confirm()
-{
-  if (document.getElementById('userpassword').value ==
-      document.getElementById('passconfirm').value)
-      {
-        document.getElementById('check').style.color = 'green';
-        document.getElementById('check').innerHTML = '&#10004';
-      }
-      else
-      {
-        document.getElementById('check').style.color = 'red';
-        document.getElementById('check').innerHTML = '&#10060';
-      }
-}
+  {
+    if (document.getElementById('userpassword').value ==
+        document.getElementById('passconfirm').value)
+        {
+          document.getElementById('check').style.color = 'green';
+          document.getElementById('check').innerHTML = '&#10004';
+        }
+        else
+        {
+          document.getElementById('check').style.color = 'red';
+          document.getElementById('check').innerHTML = '&#10060';
+        }
+  }
+
+function loginlogout()
+  {
+    document.getElementById('login').classList.toggle('hidden');
+    document.getElementById('logout').classList.toggle('hidden');
+  }
