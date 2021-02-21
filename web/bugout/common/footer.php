a505766ad97c34b7f7ba38648c7511385bee3b6b
Melissa Hull <melissahull87@gmail.com>
2021-02-20 23:43:14 -0600

    hgtr

--- web/bugout/common/footer.php
+++ web/bugout/common/footer.php
@@ -1,6 +1,6 @@
 <footer>
   <hr>
-  <span>&copy; <span id="year"></span> | Created By: Melissa Hull | Bug Out Survival |</span>
+  <span class="center">&copy; <span id="year"></span> | Created By: Melissa Hull | Bug Out Survival |</span>
         <span id="date"></span>
 </footer>
 <script type="text/javascript" src="/bugout/js/footer.js"></script>
