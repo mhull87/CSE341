eebd7c8ed50fd744e65aac05e9d71b77854b8de9
Melissa Hull <melissahull87@gmail.com>
2021-02-20 23:40:12 -0600

    aadf

---
+++ web/bugout/js/footer.js
@@ -0,0 +1,15 @@
+var d = new Date();
+
+//copyright year
+document.getElementById("year").textContent = d.getFullYear();
+
+//Current date on footer
+var dow = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+var day = dow[d.getDay()];
+var number = d.getDate();
+var m = month[d.getMonth()];
+var year = d.getFullYear();
+
+document.getElementById("date").innerHTML = (day + ", " + number + " " + m + " " + year);
